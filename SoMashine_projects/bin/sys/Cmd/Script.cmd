7fe2f73e9985bbf7e64724f77717ccbfa3c045e6
Plinfa8\Plinfa8 <20flint12@gmail.com>
2017-07-21 15:41:51 +0300

    pics+

---
+++ SoMashine_projects/bin/sys/Cmd/Script.cmd
@@ -0,0 +1,6 @@
+Download "/usr/App/Application.app"
+Download "/usr/App/Application.crc"
+Download "/usr/App/Application.app"
+Download "/usr/Application.crc"
+Download "/usr/App/Application.app"
+Download "/usr/App/Application.crc"
